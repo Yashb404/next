3a0f9321050af66d2b30992e57554e359e458084
Yash <yashbhardwaj7890@gmail.com>
2025-05-25 20:29:51 +0530

    setup and some models

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+    throw new Error("Please define mongodb uri in env file");
+}
+
+let cached = globalThis.mongoose;
+
+if (!cached) {
+    cached = globalThis.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase(): Promise<Mongoose> {
+    if (!cached) throw new Error("Mongoose cache not initialized"); // extra safety
+
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts);
+    }
+
+    try{
+    cached.conn = await cached.promise;
+    } catch (error){
+        cached.promise = null;
+        throw error
+         
+    }
+
+    return cached.conn;
+}
